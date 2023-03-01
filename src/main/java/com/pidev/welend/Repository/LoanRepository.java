fb878d440daffeeef3895db88ad3cd1c5b52a6fe
emir52 <73962635+emir52@users.noreply.github.com>
2023-03-01 11:06:42 +0100

    b

--- src/main/java/com/pidev/welend/Repository/LoanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pidev.welend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.pidev.welend.entities.Loan;
-public interface LoanRepository extends JpaRepository <Loan,Integer>{
-
-}
