2eb866fde63432135bea7715205364bad1faebea
MyesemKh <meysem.khedimi@esprit.tn>
2023-03-06 18:57:39 +0100

    init
    last

---
+++ src/main/java/com/pidev/welend/services/ReclaimServiceImp.java
@@ -0,0 +1,42 @@
+package com.pidev.welend.services;
+
+
+import com.pidev.welend.entities.Reclaim;
+import com.pidev.welend.repos.ReclaimRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ReclaimServiceImp implements ReclaimService{
+    @Autowired
+    ReclaimRepo reclaimRepo;
+
+
+    public Reclaim addReclaim(Reclaim r) {
+        return reclaimRepo.save(r);
+    }
+
+    public Reclaim updateReclaim(Reclaim r)
+    {
+        return reclaimRepo.save(r);
+    }
+
+    @Override
+    public List<Reclaim> getAllReclaim() {
+        return reclaimRepo.findAll();
+    }
+
+    @Override
+    public Reclaim getReclaimByID(Integer reclaimID) {
+        return reclaimRepo.findById(reclaimID).orElse(null);
+    }
+
+    @Override
+    public void deleteReclaim(Integer reclaimID) {
+        reclaimRepo.deleteById(reclaimID);
+
+    }
+
+
+}
